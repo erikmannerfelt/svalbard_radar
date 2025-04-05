98c9c767dfce7cfd1f21a9f9dd0fc7b8a2da2f3f
Erik Mannerfelt <33550973+erikmannerfelt@users.noreply.github.com>
2025-04-05 12:15:23 +0200

    Added stretching to one radargram.

--- format_radargrams.py
+++ format_radargrams.py
@@ -70,6 +70,7 @@ def parse_radargram(src_filepath: Path, chunksize: int = 1000, override_cache: b
         "etonbreen-20240503-DAT_0011_A1_1": 3,
         "bergmesterbreen-20230222-DAT_0017_A1_4": 3,
         "bergmesterbreen-20230222-DAT_0036_A1_1": 2,
+        "dronbreen-20250325-DAT_0029_A1_1": 3,
     }
 
     if meta_cache_path.is_file() and not override_cache:
