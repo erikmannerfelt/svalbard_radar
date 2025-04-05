ed7270bb4dfefba322e03cfee1b7e4a49e616c19
Erik Mannerfelt <33550973+erikmannerfelt@users.noreply.github.com>
2025-04-05 12:15:39 +0200

    Added submission backup script.

---
+++ backup_submissions.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+date="$(TZ="UTC" date +%Y-%m-%dT%H-%M-%SZ)"
+
+target_dir="$HOME/svalbard_radar_backups/"
+
+if ! [[ -d "$target_dir" ]]; then
+  mkdir -p "$target_dir"
+fi
+
+zip -r9 "$target_dir/svalbard_radar_backup_$date.zip" submitted/
