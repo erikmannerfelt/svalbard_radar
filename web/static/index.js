1a295fffb9c88c78853c8f1cda11f57bdebb2491
Erik Mannerfelt <33550973+erikmannerfelt@users.noreply.github.com>
2025-07-07 18:38:40 +0200

    Changed progression to a game-like leveling system.

--- web/static/index.js
+++ web/static/index.js
@@ -1,44 +1,92 @@
-async function set_progress(progress) {
+async function set_progress(n_radargrams_done, n_total_radargrams) {
+
+  let progress = n_radargrams_done / n_total_radargrams;
+
+  progress = Math.random();
+  // progress =0.9;
+  n_radargrams_done = Math.round(progress * n_total_radargrams);
 
   let progressElement = document.getElementById('progress-bar');
   let percentageElement = document.getElementById('progress-percentage');
   let progressRankElement = document.getElementById("progress-rank");
+  let current_rank_element = document.getElementById("progress-status");
   let progressPercentage = (progress * 100).toFixed(0);
     
-  progressElement.style.width = progressPercentage + '%';
-  percentageElement.innerText = progressPercentage + '%';
   
+
+  let missions = [
+    [8, "Contributor"],
+    [10, "Enthusiast"],
+    [10, "Wave"],
+    [10, "Apprentice"],
+    [10, "Investigator"],
+    [10, "Professional"],
+    [10, "Technician"],
+    [10, "Specialist"],
+    [10, "Elite"],
+    [10, "Expert"],
+    [10, "Guru"],
+    [10, "Whisperer"],
+    [200, "God"],
+  ];
+
+  let rank_name = "Beginner";
+  let mission_i = 0;
+  let mission_name;
+  let mission_points;
+  let current_zero = 0;
+  let current_goal = 0;
+  for ([mission_points, mission_name] of missions) {
+    mission_i += 1;
+
+    current_zero = current_goal;
+    // current_goal = Math.min(current_zero + mission_points, n_total_radargrams);
+    mission_points = Math.min(current_zero + mission_points, n_total_radargrams) - current_zero; 
+    current_goal = current_zero + mission_points;
+
+    if (current_goal > n_radargrams_done) {
+      break;
+    };
+    rank_name = mission_name;
+  };
+
+  let mission_score = n_radargrams_done - current_zero;
+
+  let mission_percentage = 100 * mission_score / mission_points;
+
+  progressElement.style.width = mission_percentage + '%';
+
+  let text = "";
+  if (mission_score < mission_points) {
+    text += `Mission ${mission_i}: become a Radar ${mission_name}`;
+  } else {
+    text += `You are a Radar ${mission_name}`;
+  };
+
+  text += ` (${mission_score} / ${mission_points})\n\nIn total: ${n_radargrams_done}/${n_total_radargrams} (${progressPercentage}%)`
+
+  current_rank_element.innerText = `Status: Radar ${rank_name}`;
+  progressRankElement.innerText = text;
+
+  percentageElement.innerText = `(${mission_score} / ${mission_points})`;
+
   // Change color at specific intervals
-  if (progressPercentage >= 80) {
+  if (mission_percentage >= 70) {
       progressElement.classList.add("index-card-finished");
+      console.log(progressElement);
       // progressElement.style.backgroundColor = 'green';
-  } else if (progressPercentage >= 20) {
+  } else if (mission_percentage >= 20) {
       progressElement.classList.add("index-card-unfinished");
   } else {
       progressElement.classList.add("progress-bar-low");
   }
+  // for (let rank of ranks) {
+  //   if (progressPercentage >= rank[0]) {
+  //     progressRankElement.innerText = `${rank[1]} (>${rank[0]}%)`;
+  //     break;
+  //   }
 
-  let ranks = [
-    [100, "Perfectionist radar god"],
-    [98, "Radar god"],
-    [90, "Senior radioglaciologist"],
-    [75, "Professional radioglaciologist"],
-    [50, "Halfway to perfection"],
-    [30, "Radar connoisseur"],
-    [15, "Very useful contributor!"],
-    [7, "Useful contributor"],
-    [1, "Great start!"],
-    [0, ""],
-
-  ];
-
-  for (let rank of ranks) {
-    if (progressPercentage >= rank[0]) {
-      progressRankElement.innerText = `${rank[1]} (>${rank[0]}%)`;
-      break;
-    }
-
-  };
+  // };
 
 
 };
@@ -82,7 +130,7 @@ async function setup_map() {
           }
         };
 
-        set_progress(n_total_submissions / n_total_radargrams);
+        set_progress(n_total_submissions, n_total_radargrams);
        
       };
       for (let card of document.getElementsByClassName("index-card")) {
