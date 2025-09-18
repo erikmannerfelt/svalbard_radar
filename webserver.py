8e3b6c4b850cf4405e6f28d047282a0d98f7e5c4
Erik Mannerfelt <33550973+erikmannerfelt@users.noreply.github.com>
2025-09-18 16:37:32 +0200

    Changed recommended radargram.

--- webserver.py
+++ webserver.py
@@ -395,7 +395,7 @@ def _recommended() -> list[list[str]]:
         "rabotbreen-20250402-DAT_0071_A1_1",
         "dronbreen-20240209-DAT_0463_A1_3",
         "edvardbreen-20230305-DAT_0232_A1_1",
-        "bergmesterbreen-20230222-DAT_0016_A1_1",
+        "vallakrabreen-20220419-DAT_0067_A1_1",
         "dronbreen-20240209-DAT_0466_A1_1",
         "dronbreen-20250327-DAT_0065_A1_1",
     ]
